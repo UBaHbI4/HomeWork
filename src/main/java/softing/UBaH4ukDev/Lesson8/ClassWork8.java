da06513168d12553faa6909ae7e26fb549d6b881
UBaHbI4 <kinoshnik_ivan@mail.ru>
2021-01-29 17:21:13 +0700

    Добавление пакета для урока №8.

---
+++ src/main/java/softing/UBaH4ukDev/Lesson8/ClassWork8.java
@@ -0,0 +1,13 @@
+package softing.UBaH4ukDev.Lesson8;
+
+/****
+ Project HomeWork
+ Package softing.UBaH4ukDev.Lesson8
+
+ Created by Ivan Sheynmaer
+
+ 2021.01.29
+ v1.0
+ */
+public class ClassWork8 {
+}
