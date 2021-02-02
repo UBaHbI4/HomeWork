01cbe95275a3a48b8b45fddc02379f93e80d98f0
UBaHbI4 <kinoshnik_ivan@mail.ru>
2021-02-02 14:27:33 +0700

    Подгрузили драйвер для работа с SQLite. Для этого добавили в Gradle:     // https://mvnrepository.com/artifact/org.xerial/sqlite-jdbc     compile group: 'org.xerial', name: 'sqlite-jdbc', version: '3.34.0' и запустили синхронизацию.

--- src/main/java/softing/UBaH4ukDev/Lesson9/ClassWork9.java
+++ src/main/java/softing/UBaH4ukDev/Lesson9/ClassWork9.java
@@ -1,5 +1,10 @@
 package softing.UBaH4ukDev.Lesson9;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 /****
  Project HomeWork
  Package softing.UBaH4ukDev.Lesson9
@@ -10,4 +15,35 @@ package softing.UBaH4ukDev.Lesson9;
  v1.0
  */
 public class ClassWork9 {
+    private static Connection connection;
+    private static Statement statement;
+
+    public static void main(String[] args) {
+        //Вызываем метод для соединения с БД
+        connect();
+
+        try {
+            statement = connection.createStatement();
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+
+        System.out.println("Метод сработал");
+    }
+
+    //Метод создания(если отсутствует) и соединения с БД lesson.db
+    private static void connect() {
+        try {
+            Class.forName("org.sqlite.JDBC");
+            connection = DriverManager.getConnection("jdbc:sqlite:lesson.db");
+        }
+        catch (ClassNotFoundException e) {
+            System.out.println(e.getMessage());
+        }
+        catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+
 }
