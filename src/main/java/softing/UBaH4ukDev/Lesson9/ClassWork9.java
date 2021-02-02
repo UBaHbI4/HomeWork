9291c9e0553b57816e86d006e7960f319edbe859
UBaHbI4 <kinoshnik_ivan@mail.ru>
2021-02-02 14:03:18 +0700

    Добавлен пакет для Урока №9. Добавлен пустой класс ClassWork9 для выполнения классной работы

---
+++ src/main/java/softing/UBaH4ukDev/Lesson9/ClassWork9.java
@@ -0,0 +1,13 @@
+package softing.UBaH4ukDev.Lesson9;
+
+/****
+ Project HomeWork
+ Package softing.UBaH4ukDev.Lesson9
+
+ Created by Ivan Sheynmaer
+
+ 2021.02.02
+ v1.0
+ */
+public class ClassWork9 {
+}
